4f18d944676482510f4f96d1e81ff4b38aec182e
Yfopla <ekblecker@gmail.com>
2020-03-30 19:37:19 -0400

    Added: Inquirer questions

--- app.js
+++ app.js
@@ -8,5 +8,54 @@ const fs = require("fs");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​const render = require("./lib/htmlRenderer");
 ​
-​
+​// 3 const for inquirer to get the required infomation
+const managerQuestions = [{
+     
+    message: "Enter the manager's name:",
+    name: 'email'
+    
+ }, {
+    message: "Enter the manager's Id:",
+    name: 'id'
+   
+}, {    
+    message: "Enter the manager's email:",
+    name: 'name'
+}, {
+    message: "Enter the manager's office number:",
+    name: 'officeNumber'
+}];
+
+const engineerQuestions =  [{
+    
+    message: "Enter the engineer's name:",
+    name: 'name'
+}, {
+    message: "Enter the engineer's Id:",
+    name: 'id'
+},{
+    message: "Enter the engineer's email:",
+    name: 'email'
+},{
+    message: "Enter the engineer's Github username:",
+    name: 'github'
+}]
+
+const internQuestions = [{
+
+    message: "Enter the intern's name:",
+    name: 'name'
+},
+{
+    message: "Enter the intern's Id:",
+    name: 'id'
+},
+{
+    message: "Enter the intern's email:",
+    name: 'email'
+},
+{
+    message: "Enter the intern's school:",
+    name: 'school'
+}]
 
